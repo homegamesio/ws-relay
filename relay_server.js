bd77d667cf35430fb1570924813ac220c3a9fed5
Joseph Garcia <prosif@gmail.com>
2022-12-27 09:57:41 -0700

    wip

--- relay_server.js
+++ relay_server.js
@@ -6,10 +6,6 @@ const { WebSocket, WebSocketServer } = require('ws');
 const crypto = require('crypto');
 const process = require('process');
 
-console.log("UHHHH");
-console.log(process.env.CERT_PATH);
-console.log(process.env.KEY_PATH);
-
 const server = process.env.CERT_PATH && process.env.KEY_PATH ? https.createServer({ key: fs.readFileSync(process.env.KEY_PATH), cert: fs.readFileSync(process.env.CERT_PATH) }) : http.createServer();
 
 const broadcast =  process.env.CERT_PATH && process.env.KEY_PATH ? https.createServer({ key: fs.readFileSync(process.env.KEY_PATH), cert: fs.readFileSync(process.env.CERT_PATH) }) : http.createServer();
@@ -27,6 +23,8 @@ let _serverId = 1;
 const generateCode = () => crypto.randomBytes(2).toString('hex').toUpperCase();
 
 const sessionCodes = {};
+
+const internalToClientIds = {};
 
 wss.on('connection', (ws) => {
     const serverId = _serverId++;
@@ -49,9 +47,50 @@ wss.on('connection', (ws) => {
     });
 
     ws.on('message', (message) => {
-        Object.keys(clients).forEach(id => {
-            clients[id] && clients[id].send(message);
-        });
+	if (message.toString().startsWith('gimmeidresponse-')) {
+		console.log('got id response thing');
+		console.log(message);
+		const pieces = message.toString().split('-');
+		if (pieces.length == 3) {
+			internalToClientIds[pieces[1]] = pieces[2];
+			console.log('id map');
+			console.log(internalToClientIds);
+			const clientToNotify = clients[pieces[1]];
+			clientToNotify.send([2, pieces[2]]);
+		}
+	} else {
+		if (message[0] === 2) {
+		// init message, pull client id
+			const lol = {};
+			for (let key in internalToClientIds) {
+				lol[Number(internalToClientIds[key])] = Number(key);
+			}
+			const serverClientId = message[1];
+			console.log('looks like im sending more info about this ' + serverClientId);
+			console.log(lol);
+			console.log(internalToClientIds);
+			const realClient = clients[lol[serverClientId]];
+			realClient.send(message);
+		} else if (message[0] === 199) {
+		    // standard proxy message, pull client id
+		    console.log(internalToClientIds);	
+			const lol = {};
+			for (let key in internalToClientIds) {
+				lol[Number(internalToClientIds[key])] = Number(key);
+			}
+			const serverClientId = message[1];
+		        console.log('looks like im passing a message to this ' + serverClientId);
+			console.log(internalToClientIds);
+			const realClient = clients[lol[serverClientId]];
+			realClient.send(message.slice(2));
+	
+		} else {
+		// something else
+                    Object.keys(clients).forEach(id => {
+                        clients[id] && clients[id].send(message);
+                    });
+		}
+	}
     });
 });
 
@@ -61,6 +100,7 @@ broadcastServer.on('connection', (ws) => {
     const wsId = id++;
     clients[wsId] = ws;
     let clientCode;
+    let clientId;
     let connectedHgServer;
     ws.on('message', (message) => {
 	// first message should be code
@@ -73,12 +113,19 @@ broadcastServer.on('connection', (ws) => {
 		  ws.send('error: bad code supplied');
 		  ws.close();
 		} else {
+			console.log('just did this. requesting player ID from game server');
+//			ws.send(JSON.stringify({type: 'id', payload: '' + wsId}));
 			connectedHgServer = servers[requestedServerId];
+			connectedHgServer.send('gimmeid-' + wsId);
 		}
 	    } else {
 		console.log('bad message from public client');
 		console.log(codePayload);
 	    }
+	//}// else if (!clientId) {
+	//	console.log('please tell me this is an ayylmao message');
+	//	console.log(message);
+	//	console.log(message.toString());
 	} else {
 	   if (!connectedHgServer) {
 		console.error('not connected to hg server');
@@ -95,5 +142,3 @@ broadcastServer.on('connection', (ws) => {
 
 server.listen(81);
 broadcast.listen(82);
-
-
