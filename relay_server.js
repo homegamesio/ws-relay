00923906306e5bd62a723a09cdcb6aa3141089a2
Joseph Garcia <prosif@gmail.com>
2022-12-28 08:31:39 -0700

    closing connection stuff

--- relay_server.js
+++ relay_server.js
@@ -1,7 +1,8 @@
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 const fs = require('fs');
-const { WebSocketServer } = require('ws');
+const path = require('path');
+const { WebSocket, WebSocketServer } = require('ws');
 const crypto = require('crypto');
 const process = require('process');
 
@@ -25,10 +26,15 @@ const sessionCodes = {};
 
 const internalToClientIds = {};
 
+// which server is a person in
+const consumerIdToServerId = {};
+
 wss.on('connection', (ws) => {
     const serverId = _serverId++;
     servers[serverId] = ws;
     internalToClientIds[serverId] = {};
+
+    console.log('a broadcaster just connected to me');
 
     const serverCode = generateCode();
     sessionCodes[serverCode] = serverId;
@@ -41,48 +47,49 @@ wss.on('connection', (ws) => {
 
     ws.on('close', () => {
         servers[serverId] = null;
-        sessionCodes[serverCode] = null;
+	sessionCodes[serverCode] = null;
     });
 
     ws.on('message', (message) => {
-        if (message.toString().startsWith('gimmeidresponse-')) {
-            const pieces = message.toString().split('-');
-            if (pieces.length == 3) {
-                if (!internalToClientIds[serverId]) {
-                    internalToClientIds[serverId] = {};
-                }
-                internalToClientIds[serverId][pieces[1]] = pieces[2];
-                const clientToNotify = clients[pieces[1]];
-                clientToNotify.send([2, pieces[2]]);
-            }
-        } else {
-            if (message[0] === 2) {
-                // init message, pull client id
-                const lol = {};
-                for (const key in internalToClientIds[serverId]) {
-                    lol[Number(internalToClientIds[serverId][key])] = Number(key);
-                }
-                const serverClientId = message[1];
-                const realClient = clients[lol[serverClientId]];
-                realClient && realClient.send(message);
-            } else if (message[0] === 199) {
-            // standard proxy message, pull client id
-                const lol = {};
-                for (const key in internalToClientIds[serverId]) {
-                    lol[Number(internalToClientIds[serverId][key])] = Number(key);
-                }
+	if (message.toString().startsWith('gimmeidresponse-')) {
+		const pieces = message.toString().split('-');
+		if (pieces.length == 3) {
+			if (!internalToClientIds[serverId]) {
+				internalToClientIds[serverId] = {};
+			}
+			internalToClientIds[serverId][pieces[1]] = pieces[2];
+			consumerIdToServerId[pieces[1]] = serverId;
+			const clientToNotify = clients[pieces[1]];
+			clientToNotify.send([2, pieces[2]]);
+		}
+	} else {
+		if (message[0] === 2) {
+		// init message, pull client id
+			const lol = {};
+			for (let key in internalToClientIds[serverId]) {
+				lol[Number(internalToClientIds[serverId][key])] = Number(key);
+			}
+			const serverClientId = message[1];
+			const realClient = clients[lol[serverClientId]];
+			realClient && realClient.send(message);
+		} else if (message[0] === 199) {
+		    // standard proxy message, pull client id
+			const lol = {};
+			for (let key in internalToClientIds[serverId]) {
+				lol[Number(internalToClientIds[serverId][key])] = Number(key);
+			}
 
-                const serverClientId = message[1];
-                const realClient = clients[lol[serverClientId]];
-                realClient && realClient.send(message.slice(2));
-    
-            } else {
-                // something else
-                Object.keys(clients).forEach(id => {
-                    clients[id] && clients[id].send(message);
-                });
-            }
-        }
+			const serverClientId = message[1];
+			const realClient = clients[lol[serverClientId]];
+			realClient && realClient.send(message.slice(2));
+	
+		} else {
+		// something else
+                    Object.keys(clients).forEach(id => {
+                        clients[id] && clients[id].send(message);
+                    });
+		}
+	}
     });
 });
 
@@ -90,39 +97,78 @@ let id = 1;
 broadcastServer.on('connection', (ws) => {
     const wsId = id++;
     clients[Number(wsId)] = ws;
-
     let clientCode;
     let connectedHgServer;
-
     ws.on('message', (message) => {
-        // first message should be code
-        if (!clientCode) {
-            const codePayload = JSON.parse(message);
-            if (codePayload.type === 'code') {
-                clientCode = codePayload.code;
-                const requestedServerId = sessionCodes[clientCode];
-                if (!requestedServerId || !servers[requestedServerId]) {
-                    ws.send('error: bad code supplied');
-                    ws.close();
-                } else {
-                    connectedHgServer = servers[requestedServerId];
-                    connectedHgServer.send('gimmeid-' + wsId);
-                }
-            } else {
-                console.log('bad message from public client');
-                console.log(codePayload);
-            }
-        } else {
-            if (!connectedHgServer) {
-                console.error('not connected to hg server');
-            } else {
-                connectedHgServer.send(message);
-            }
-        }
+	// first message should be code
+	if (!clientCode) {
+	    const codePayload = JSON.parse(message);
+	    if (codePayload.type === 'code') {
+		clientCode = codePayload.code;
+		const requestedServerId = sessionCodes[clientCode];
+		if (!requestedServerId || !servers[requestedServerId]) {
+		  ws.send('error: bad code supplied');
+		  ws.close();
+		} else {
+//			ws.send(JSON.stringify({type: 'id', payload: '' + wsId}));
+			connectedHgServer = servers[requestedServerId];
+			connectedHgServer.send('gimmeid-' + wsId);
+		}
+//	    }
+		//else if (codePayload.type === 'ready') {
+		//    console.log('dasfsdfsdfdsf');
+		//    console.log(codePayload);
+		//// reconnecting from existing known session (eg. spectator to player or vice versa)
+		//if (codePayload.code) {
+		//	console.log('dsfdsfdsf');
+		//	console.log(codePayload);
+		//	clientCode = codePayload.code;
+		//	const requestedServerId = sessionCodes[clientCode];
+		//	if (!requestedServerId || !servers[requestedServerId]) {
+		//  		ws.send('error: bad code supplied');
+		//  		ws.close();
+		//	} 
+		//	console.log('sending this here to the thing');
+		//	console.log(message.toString());
+		//	connectedHgServer = servers[requestedServerId];
+		//	connectedHgServer.send('gimmeid-' + wsId);
+	
+                //	servers[requestedServerId].send(message);
+	
+		//}
+	    } else {
+		console.log('bad message from public client');
+		console.log(codePayload);
+	    }
+	} else {
+	   if (!connectedHgServer) {
+		console.error('not connected to hg server');
+	   } else {
+		console.log('oh okay');
+		   console.log(message);
+		   console.log(message.toString());
+	const connectedServerId = consumerIdToServerId[wsId];
+	if (connectedServerId) {
+		const serverClientId = internalToClientIds[connectedServerId][wsId];
+		console.log('server client id??????? ' + serverClientId);
+                connectedHgServer.send([serverClientId, ...message]);
+	}
+	
+	   }
+	}
     });
 
     ws.on('close', () => {
         clients[wsId] = null;
+	const connectedServerId = consumerIdToServerId[wsId];
+	if (connectedServerId) {
+		const serverClientId = internalToClientIds[connectedServerId][wsId];
+		console.log('server client id ' + serverClientId);
+		connectedHgServer && connectedHgServer.send('close-' + serverClientId);
+		delete internalToClientIds[connectedServerId][wsId];
+		delete consumerIdToServerId[wsId];
+	}
+	console.log('need to send close to hg server');
     });
 });
 
